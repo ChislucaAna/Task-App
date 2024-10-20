3a309eba4d1bc0e2907bcc21e72aae8ffbf7d7d0
Nunu <ana.chisluca.code@gmail.com>
2024-10-20 12:12:46 +0300

    Created task model and registered it in admin

--- tasksite/manager/models.py
+++ tasksite/manager/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    PRIORITY_CHOICES = [ #prima coloana e db value a doua e human value
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    due_date = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField()
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, default='low')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
